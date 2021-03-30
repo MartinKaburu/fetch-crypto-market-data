8029b9d226254ae82f6d7b3c38762167441703bf
martinkaburu <martinkaburu.m@gmail.com>
2021-03-31 00:37:42 +0300

    ft: generate pairs and write to file

--- main.go
+++ main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 type cryptoExchangeAPI string
 
 var cryptoExchangeAPIMap = map[string]cryptoExchangeAPI{"binance": "https://api1.binance.com/api/v3/exchangeInfo", "ftx": "https://ftx.com/api/markets", "kraken": "https://api.kraken.com/0/public/AssetPairs"}
 
-func makeAPICall(url cryptoExchangeAPI, provider string) interface{} {
+type Currency struct {
+	BaseCurrency  string
+	QuoteCurrency string
+}
+
+func makeAPICall(url cryptoExchangeAPI, provider string) []Currency {
 	r, err := http.Get(string(url))
 	if err != nil {
-		return err
+		panic(err.Error())
 	}
 	defer r.Body.Close()
 	body, err := ioutil.ReadAll(r.Body)
@@ -33,7 +40,11 @@ func makeAPICall(url cryptoExchangeAPI, provider string) interface{} {
 		if err != nil {
 			panic(err.Error())
 		}
-		return data
+		slc := make([]Currency, 0, len(data.Result))
+		for _, ob := range data.Result {
+			slc = append(slc, Currency(ob))
+		}
+		return slc
 	case "ftx":
 		var data struct {
 			Result []struct {
@@ -45,7 +56,11 @@ func makeAPICall(url cryptoExchangeAPI, provider string) interface{} {
 		if err != nil {
 			panic(err.Error())
 		}
-		return data
+		slc := make([]Currency, 0, len(data.Result))
+		for _, ob := range data.Result {
+			slc = append(slc, Currency(ob))
+		}
+		return slc
 	case "binance":
 		var data struct {
 			Result []struct {
@@ -57,15 +72,35 @@ func makeAPICall(url cryptoExchangeAPI, provider string) interface{} {
 		if err != nil {
 			panic(err.Error())
 		}
-		return data
+		slc := make([]Currency, 0, len(data.Result))
+		for _, ob := range data.Result {
+			slc = append(slc, Currency(ob))
+		}
+		return slc
 	}
-	var data interface{}
+	var data []Currency
 	return data
 }
 
+func generatePairs(data []Currency) string {
+	var buffer bytes.Buffer
+	for _, obj := range data {
+		buffer.WriteString(fmt.Sprintf("%s/%s\n", obj.BaseCurrency, obj.QuoteCurrency))
+	}
+	return buffer.String()
+}
+
 func main() {
+	f, err := os.OpenFile("text.log",
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err.Error())
+	}
 	for provider, url := range cryptoExchangeAPIMap {
-		fmt.Println(">>>>>>>>>>>>>>>>>>", provider, makeAPICall(url, provider))
+		pairs := generatePairs(makeAPICall(url, provider))
+		if _, err = f.WriteString(pairs); err != nil {
+			panic(err.Error())
+		}
 	}
 
 }
